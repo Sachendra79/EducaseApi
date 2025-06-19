18938a2e1d0b6179106676bb07e2829f1c9f0f17
Sachendra79 <sachendragangwar120@gmail.com>
2025-06-19 14:40:14 +0530

    completed project

--- config/database.js
+++ config/database.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
+console.log("Connecting to DB with:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT
+});
+
 module.exports = pool.promise();
+
