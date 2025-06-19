80a8f033af2419872fcccecfdc74645a2585b9b0
Sachendra79 <sachendragangwar120@gmail.com>
2025-06-19 13:28:14 +0530

    added the reqired controller

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -3,15 +3,19 @@ const { body, query } = require('express-validator');
 
 
 
-const router = express.Router();
+
+
 const {
   addSchool,
   listSchools
 } = require('../controller/schoolController');
 
-// POST /api/v1/schools
+const router = express.Router();
+
+
+// POST /api/v1/schools/addSchool
 router.post(
-  '/',
+  '/addSchool',
   [
     body('name').notEmpty().withMessage('Name is required'),
     body('address').notEmpty().withMessage('Address is required'),
@@ -21,9 +25,9 @@ router.post(
   addSchool
 );
 
-// GET /api/v1/schools?latitude=..&longitude=..
+// GET /api/v1/schools/listSchools?latitude=...&longitude=...
 router.get(
-  '/',
+  '/listSchools',
   [
     query('latitude').isFloat().withMessage('Latitude must be a float'),
     query('longitude').isFloat().withMessage('Longitude must be a float')
